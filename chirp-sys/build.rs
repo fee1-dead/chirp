de0c216f851998be76eb7afef1a43cc7df4f7452
Deadbeef <ent3rm4n@gmail.com>
2024-08-09 21:41:36 +0800

    link more libraries needed

--- chirp-sys/build.rs
+++ chirp-sys/build.rs
@@ -11,10 +11,19 @@ fn main() {
         .build();
 
     println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=common_rhi");
+    println!("cargo:rustc-link-lib=static=llvm_rhi");
+    println!("cargo:rustc-link-lib=static=llvm_program_impl");
     println!("cargo:rustc-link-lib=static=taichi_core");
     println!("cargo:rustc-link-lib=static=taichi_common");
     println!("cargo:rustc-link-lib=static=taichi_util");
     println!("cargo:rustc-link-lib=static=ti_device_api");
+    println!("cargo:rustc-link-lib=static=opengl_rhi");
+    println!("cargo:rustc-link-lib=static=opengl_program_impl");
+    println!("cargo:rustc-link-lib=static=cuda_rhi");
+    //println!("cargo:rustc-link-lib=static=opengl_rhi");
+    //println!("cargo:rustc-link-lib=static=opengl_rhi");
+    // println!("cargo:rustc-link-lib=static=llvm");
 
     let include = dst.join("include");
 
